ab635d6808a14b7c604a684a696c6e3407c26698
Youssef Chahbi <ychahbi@e1r7p8.1337.ma>
2022-11-16 13:33:34 +0100

    125

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:25:46 by ychahbi           #+#    #+#             */
-/*   Updated: 2022/11/03 15:38:02 by ychahbi          ###   ########.fr       */
+/*   Updated: 2022/11/16 13:29:32 by ychahbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,10 +96,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 
 t_list	*ft_lstnew(void *content);
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
-void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
